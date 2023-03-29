7c52a1a24664f2a92f26d6bd924d07ffae0addb0
dipshen <di.pshennikova@gmail.com>
2023-03-29 14:35:16 +0300

    5

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -12,11 +12,26 @@ $(document).ready(function(){
 	$(".fish1").click(function() {
  		 $(".main").addClass("none");
 		 $(".second").removeClass("none")
+		 $(".blur").removeClass("none");
+		 $(".hint2_1").removeClass("none")
 	});
-
+	$(".X2").click(function() {
+		 $(".hint2_1").addClass("none");
+		 $(".blur").addClass("none");
+	});
+	$(".bottom1").click(function() {
+		 $(".bottom1").addClass("none");
+		 $(".bottom2").removeClass("none");
+	});
 	$(".fish2").click(function() {
  		 $(".main").addClass("none");
 		 $(".third").removeClass("none")
+		 $(".blur").removeClass("none");
+		 $(".hint3_1").removeClass("none")
+	});
+	$(".X3").click(function() {
+		 $(".hint3_1").addClass("none");
+		 $(".blur").addClass("none");
 	});
 
 });
