39a16cb35084cd076bf4db6cf17f315b0fa00b97
dipshen <di.pshennikova@gmail.com>
2023-03-30 18:26:32 +0300

    9

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -6,9 +6,15 @@ $(document).ready(function(){
 		 $(".blur").removeClass("none");
 		 $(".hint1_1").removeClass("none");
 	});
-	$(".X1").click(function() {
+	$(".X1_1").click(function() {
 		 $(".hint1_1").addClass("none");
 		 $(".blur").addClass("none");
+	});
+	$(".X1_2").click(function() {
+		 $(".hint1_2").addClass("none");
+		 $(".blur").addClass("none");
+		 $(".main").removeClass("none");
+		 $(".first").addClass("none");
 	});
 	$(".b1").click(function() {
 		 $(".b1").addClass("none");
@@ -70,30 +76,221 @@ $(document).ready(function(){
 	$(".b20").click(function() {
 		 $(".b20").addClass("none");
 	});
+	$(".b21").click(function() {
+		 $(".b21").addClass("none");
+	});
+	$(".b22").click(function() {
+		 $(".b22").addClass("none");
+	});
+	$(".b23").click(function() {
+		 $(".b23").addClass("none");
+	});
+	$(".b24").click(function() {
+		 $(".b24").addClass("none");
+	});
+	$(".b25").click(function() {
+		 $(".b25").addClass("none");
+	});
+	$(".b26").click(function() {
+		 $(".b26").addClass("none");
+	});
+	$(".b27").click(function() {
+		 $(".b27").addClass("none");
+	});
+	$(".b28").click(function() {
+		 $(".b28").addClass("none");
+	});
+	$(".b29").click(function() {
+		 $(".b29").addClass("none");
+	});
+	$(".b30").click(function() {
+		 $(".b30").addClass("none");
+	});
+	$(".b31").click(function() {
+		 $(".b31").addClass("none");
+	});
+	$(".b32").click(function() {
+		 $(".b32").addClass("none");
+	});
+	$(".b33").click(function() {
+		 $(".b33").addClass("none");
+	});
+	$(".b34").click(function() {
+		 $(".b34").addClass("none");
+	});
+	$(".b35").click(function() {
+		 $(".b35").addClass("none");
+	});
+	$(".b36").click(function() {
+		 $(".b36").addClass("none");
+	});
+	$(".b37").click(function() {
+		 $(".b37").addClass("none");
+	});
+	$(".b38").click(function() {
+		 $(".b38").addClass("none");
+	});
+	$(".b39").click(function() {
+		 $(".b39").addClass("none");
+	});
+	$(".b40").click(function() {
+		 $(".b40").addClass("none");
+	});
+	$(".b41").click(function() {
+		 $(".b41").addClass("none");
+	});
+	$(".b42").click(function() {
+		 $(".b42").addClass("none");
+	});
+	$(".b43").click(function() {
+		 $(".b43").addClass("none");
+	});
+	$(".b44").click(function() {
+		 $(".b44").addClass("none");
+	});
+	$(".b45").click(function() {
+		 $(".b45").addClass("none");
+	});
+	$(".b46").click(function() {
+		 $(".b46").addClass("none");
+	});
+	$(".b47").click(function() {
+		 $(".b47").addClass("none");
+	});
+	$(".b48").click(function() {
+		 $(".b48").addClass("none");
+	});
+	$(".b49").click(function() {
+		 $(".b49").addClass("none");
+	});
+	$(".b50").click(function() {
+		 $(".b50").addClass("none");
+	});
+	$(".b51").click(function() {
+		 $(".b51").addClass("none");
+	});
+	$(".b52").click(function() {
+		 $(".b52").addClass("none");
+	});
+	$(".b53").click(function() {
+		 $(".b53").addClass("none");
+	});
+	$(".b54").click(function() {
+		 $(".b54").addClass("none");
+	});
+	$(".b55").click(function() {
+		 $(".b55").addClass("none");
+	});
+	$(".b56").click(function() {
+		 $(".b56").addClass("none");
+	});
+	$(".b57").click(function() {
+		 $(".b57").addClass("none");
+	});
+	$(".b58").click(function() {
+		 $(".b58").addClass("none");
+	});
+	$(".b59").click(function() {
+		 $(".b59").addClass("none");
+	});
+	$(".b60").click(function() {
+		 $(".b60").addClass("none");
+	});
+	$(".b61").click(function() {
+		 $(".b61").addClass("none");
+	});
+	$(".b62").click(function() {
+		 $(".b62").addClass("none");
+	});
+	$(".b63").click(function() {
+		 $(".b63").addClass("none");
+	});
+	$(".b64").click(function() {
+		 $(".b64").addClass("none");
+	});
+	$(".b65").click(function() {
+		 $(".b65").addClass("none");
+	});
+	$(".b66").click(function() {
+		 $(".b66").addClass("none");
+	});
+	$(".b67").click(function() {
+		 $(".b67").addClass("none");
+	});
+	$(".b68").click(function() {
+		 $(".b68").addClass("none");
+	});
+	$(".b69").click(function() {
+		 $(".b69").addClass("none");
+	});
+	$(".b70").click(function() {
+		 $(".b70").addClass("none");
+	});
+	$(".b71").click(function() {
+		 $(".b71").addClass("none");
+	});
+	$(".b72").click(function() {
+		 $(".b72").addClass("none");
+	});
+	$(".b73").click(function() {
+		 $(".b73").addClass("none");
+	});
+	$(".b74").click(function() {
+		 $(".b74").addClass("none");
+	});
+	$(".b75").click(function() {
+		 $(".b75").addClass("none");
+	});
+	$(".b76").click(function() {
+		 $(".b76").addClass("none");
+	});
+	$(".b77").click(function() {
+		 $(".b77").addClass("none");
+	});
+	$(".b78").click(function() {
+		 $(".b78").addClass("none");
+	});
+	$(".b79").click(function() {
+		 $(".b79").addClass("none");
+	});
+	$(".b80").click(function() {
+		 $(".b80").addClass("none");
+	});
 	setInterval(function(){
 		if ($(".b1").hasClass("none") && $(".b2").hasClass("none") && $(".b3").hasClass("none")
-	// && $(".b4").hasClass("none") && $(".b5").hasClass("none") && $(".b6").hasClass("none")
-	// && $(".b7").hasClass("none") && $(".b8").hasClass("none") && $(".b9").hasClass("none")
-	// && $(".b10").hasClass("none") && $(".b11").hasClass("none") && $(".b12").hasClass("none")
-	// && $(".b13").hasClass("none") && $(".b14").hasClass("none") && $(".b15").hasClass("none")
-	// && $(".b16").hasClass("none") && $(".b17").hasClass("none") && $(".b18").hasClass("none")
-	// && $(".b19").hasClass("none") && $(".b20").hasClass("none") && $(".b21").hasClass("none")
-	// && $(".b22").hasClass("none") && $(".b23").hasClass("none") && $(".b24").hasClass("none")
-	// && $(".b25").hasClass("none") && $(".b26").hasClass("none") && $(".b27").hasClass("none")
-	// && $(".b28").hasClass("none") && $(".b29").hasClass("none") && $(".b30").hasClass("none")
-	// && $(".b31").hasClass("none") && $(".b32").hasClass("none") && $(".b33").hasClass("none")
-	// && $(".b34").hasClass("none") && $(".b35").hasClass("none") && $(".b36").hasClass("none")
-	// && $(".b37").hasClass("none") && $(".b38").hasClass("none") && $(".b39").hasClass("none")
-	// && $(".b40").hasClass("none") && $(".b41").hasClass("none") && $(".b42").hasClass("none")
-		) {
+		// && $(".b4").hasClass("none") && $(".b5").hasClass("none") && $(".b6").hasClass("none")
+		// && $(".b7").hasClass("none") && $(".b8").hasClass("none") && $(".b9").hasClass("none")
+		// && $(".b10").hasClass("none") && $(".b11").hasClass("none") && $(".b12").hasClass("none")
+		// && $(".b13").hasClass("none") && $(".b14").hasClass("none") && $(".b15").hasClass("none")
+		// && $(".b16").hasClass("none") && $(".b17").hasClass("none") && $(".b18").hasClass("none")
+		// && $(".b19").hasClass("none") && $(".b20").hasClass("none") && $(".b21").hasClass("none")
+		// && $(".b22").hasClass("none") && $(".b23").hasClass("none") && $(".b24").hasClass("none")
+		// && $(".b25").hasClass("none") && $(".b26").hasClass("none") && $(".b27").hasClass("none")
+		// && $(".b28").hasClass("none") && $(".b29").hasClass("none") && $(".b30").hasClass("none")
+		// && $(".b31").hasClass("none") && $(".b32").hasClass("none") && $(".b33").hasClass("none")
+		// && $(".b34").hasClass("none") && $(".b35").hasClass("none") && $(".b36").hasClass("none")
+		// && $(".b37").hasClass("none") && $(".b38").hasClass("none") && $(".b39").hasClass("none")
+		// && $(".b40").hasClass("none") && $(".b41").hasClass("none") && $(".b42").hasClass("none")
+		// && $(".b43").hasClass("none") && $(".b44").hasClass("none") && $(".b45").hasClass("none")
+		// && $(".b46").hasClass("none") && $(".b47").hasClass("none") && $(".b48").hasClass("none")
+		// && $(".b49").hasClass("none") && $(".b50").hasClass("none") && $(".b51").hasClass("none")
+		// && $(".b51").hasClass("none") && $(".b52").hasClass("none") && $(".b53").hasClass("none")
+		// && $(".b54").hasClass("none") && $(".b55").hasClass("none") && $(".b56").hasClass("none")
+		// && $(".b57").hasClass("none") && $(".b58").hasClass("none") && $(".b59").hasClass("none")
+		// && $(".b60").hasClass("none") && $(".b61").hasClass("none") && $(".b62").hasClass("none")
+		// && $(".b63").hasClass("none") && $(".b64").hasClass("none") && $(".b65").hasClass("none")
+		// && $(".b66").hasClass("none") && $(".b67").hasClass("none") && $(".b68").hasClass("none")
+		// && $(".b69").hasClass("none") && $(".b70").hasClass("none") && $(".b71").hasClass("none")
+		// && $(".b72").hasClass("none") && $(".b73").hasClass("none") && $(".b74").hasClass("none")
+		// && $(".b75").hasClass("none") && $(".b76").hasClass("none") && $(".b77").hasClass("none")
+		// && $(".b78").hasClass("none") && $(".b79").hasClass("none") && $(".b80").hasClass("none")
+		&& $(".main").hasClass("none");
+		&& $(".second").hasClass("none");
+		&& $(".third").hasClass("none");) {
 			$(".blur").removeClass("none");
-			$(".hint1_2").removeClass("none");
-	}, 1000);
-	// if ($('.b1, .b2, .b3').hasClass('none').length === 3) {
-  // 	$('.blur').removeClass('none');
-	// };
-
-	// ВТОРАЯ ИНРА
+			$(".hint1_2").removeClass("none");}
+	}, 7000);
+	// ВТОРАЯ ИГРА
 	$(".fish1").click(function() {
  		 $(".main").addClass("none");
 		 $(".second").removeClass("none")
